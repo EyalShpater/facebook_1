659dbd7f58dc9d4b0dc8e58ed7fdb7613d80b6ca
Eyal Shpater <shpaters@hotmail.com>
2022-11-19 22:02:03 +0200

    add a Time var and Date.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,11 @@ using namespace std;
 int main()
 {
     cout << "hello" << endl;
-    Time t(19, 11, 2021);
-    Date();
+    Time t(22, 00, 32);
+    Date d(19, 11, 2021);
+
+    t.show();
+    d.show();
+    
     return 0;
 }
